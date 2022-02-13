a3b51b2f2fcc85677eb8a24d4f5c6a298f2c54fa
Mustafa Yalçın <87071995+fizyomustafa@users.noreply.github.com>
2022-02-14 01:39:06 +0300

    Delete Ios-Calculator directory

--- Ios-Calculator/main.js
+++
@@ -1,9 +0,0 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-let clock = () => {
-  hour.innerHTML = new Date().getHours().toString().padStart(2, "0");
-  minute.innerHTML = new Date().getMinutes().toString().padStart(2, "0");
-};
-
-setInterval(clock, 1000);
-
